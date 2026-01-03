a183218d9175e3a3231d5ea444c15b44865f640e
sylvessa <225480449+sylvessa@users.noreply.github.com>
2026-01-03 13:37:48 -0600

    hat

---
+++ src/sylvessa/plugin/commands/HatCommand.java
@@ -0,0 +1,40 @@
+package sylvessa.plugin.commands;
+
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+public class HatCommand implements PluginCommand {
+    public String name() {
+        return "hat";
+    }
+
+    public String description() {
+        return "Makes the current item you're holding your helmet.";
+    }
+
+    public void execute(CommandSender sender, String[] args) {
+        if(!(sender instanceof Player)) {
+            sender.sendMessage("Only players can use this command.");
+            return;
+        }
+
+        Player player = (Player) sender;
+        ItemStack hand = player.getItemInHand();
+
+        if (hand == null || hand.getTypeId() == 0) {
+            player.sendMessage("You must be holding an item.");
+            return;
+        }
+
+        ItemStack helmet = player.getInventory().getHelmet();
+
+        player.getInventory().setHelmet(hand);
+
+        if(helmet != null && helmet.getTypeId() != 0) {
+            player.setItemInHand(helmet);
+        } else {
+            player.setItemInHand(null);
+        }
+    }
+}
