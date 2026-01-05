5000ff899eeebf406c3ef7b87546b3857020670c
sylvessa <225480449+sylvessa@users.noreply.github.com>
2026-01-05 02:20:13 -0600

    WhatEVa

--- src/sylvessa/plugin/commands/TestCommand.java
+++ src/sylvessa/plugin/commands/TestCommand.java
@@ -1,5 +1,11 @@
 package sylvessa.plugin.commands;
+import me.devcody.uberbukkit.util.math.Vec3i;
+import net.minecraft.server.*;
+import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
+import org.bukkit.craftbukkit.CraftWorld;
+import org.bukkit.craftbukkit.entity.CraftPlayer;
+import org.bukkit.entity.Player;
 
 @SuppressWarnings("unused")
 public class TestCommand implements PluginCommand {
@@ -13,5 +19,7 @@ public class TestCommand implements PluginCommand {
 
     public void execute(CommandSender sender, String[] args) {
         sender.sendMessage("Whats Good " + sender.getName());
+        // pls work
+
     }
 }
