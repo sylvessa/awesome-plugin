7c1483cd820399940cfb1564fba7c9da9aeb9ebe
sylvessa <225480449+sylvessa@users.noreply.github.com>
2026-01-05 02:20:30 -0600

    WhatEVa

--- src/sylvessa/plugin/commands/TestCommand.java
+++ src/sylvessa/plugin/commands/TestCommand.java
@@ -1,11 +1,5 @@
 package sylvessa.plugin.commands;
-import me.devcody.uberbukkit.util.math.Vec3i;
-import net.minecraft.server.*;
-import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
-import org.bukkit.craftbukkit.CraftWorld;
-import org.bukkit.craftbukkit.entity.CraftPlayer;
-import org.bukkit.entity.Player;
 
 @SuppressWarnings("unused")
 public class TestCommand implements PluginCommand {
